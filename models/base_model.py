923630b0040e83a9d59c34176840bbd2e021a3ac
Anne Machinda <annemachinda@gmail.com>
2021-11-14 17:19:39 +0100

    base model

---
+++ models/base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A base model class"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """ A class BaseModel that defines all common attributes/method for other classes """
+    id = str(uuid.uuid4())
+    created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+    def __str__(self):
+        """ String representation"""
+        return self.__class__.__name__, self.id, self.__dict__.__format__("{:s} {:s} {}")
+
+    def save(self):
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return self.updated_at
+
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of __dict__ of the instance"""
